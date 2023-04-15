1e77eb17fcfff2c208236e2ee25d891cd3df76bf
Raphael Araújo <raphaelaraujotoc@outlook.com>
2023-04-15 00:37:32 -0300

    adicionando o campo participantes na tabela Evento

--- eventos/models.py
+++ eventos/models.py
@@ -5,12 +5,17 @@ from django.db import models
 class Evento(models.Model):
     criador = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     nome = models.CharField(max_length=200)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     descricao = models.TextField()
     data_inicio = models.DateField()
     data_termino = models.DateField()
     carga_horaria = models.PositiveIntegerField()
     logo = models.FileField(upload_to='logos')
+    participantes = models.ManyToManyField(
+        User,
+        related_name='evento_participante',
+        blank=True
+    )
 
     # paleta de cores:
     cor_principal = models.CharField(max_length=7)
