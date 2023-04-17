885928ac31ee84a96573d0ad290794e31f617321
Raphael Araújo <raphaelaraujotoc@outlook.com>
2023-04-17 17:25:37 -0300

    instalando o app 'clientes'

--- type_event/settings.py
+++ type_event/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # apps:
     'usuarios',
     'eventos',
+    'clientes'
 ]
 
 MIDDLEWARE = [
--- type_event/urls.py
+++ type_event/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls')),
     path('eventos/', include('eventos.urls')),
+    path('clientes/', include('clientes.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
